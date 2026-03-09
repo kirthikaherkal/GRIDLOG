858af05c9585ae250eebcaa92a0786fbd08ff81f
clutchmac <omkarmaheshputti@gmail.com>
2026-03-09 15:00:50 +0530

    checkin and checkout issue fixed

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://gridlogg.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
